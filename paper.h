4722b6fcf040cd9a4aa790f02f4ce96bf0a3432e
WilliamBuffum <buffumw@oregonstate.edu>
2015-10-22 04:22:07 -0700

    Create paper.h

---
+++ paper.h
@@ -0,0 +1,13 @@
+#ifndef PAPER_H//preprocessor guards
+#define PAPER_H
+#include "Tool.h"
+
+class Paper: public Tool{
+   public:
+      Paper();
+      Paper(int);
+      void operator=(const Paper &);
+      void set_strength(int);
+      int fight(Tool);//fight function polymorphs depending on what tool is used
+};
+#endif
